a1140137d12e22730568dff786cf2a4f20ffd469
chengzh <chngzihan11@gmail.com>
2025-12-17 22:23:05 +0800

    Commit solution for assignment5

--- assignment5/user.cpp
+++ assignment5/user.cpp
@@ -3,24 +3,17 @@
 /**
  * Creates a new User with the given name and no friends.
  */
-User::User(const std::string& name)
-  : _name(name)
-  , _friends(nullptr)
-  , _size(0)
-  , _capacity(0)
-{
-}
+User::User(const std::string &name)
+    : _name(name), _friends(nullptr), _size(0), _capacity(0) {}
 
 /**
  * Adds a friend to this User's list of friends.
  * @param name The name of the friend to add.
  */
-void
-User::add_friend(const std::string& name)
-{
+void User::add_friend(const std::string &name) {
   if (_size == _capacity) {
     _capacity = 2 * _capacity + 1;
-    std::string* newFriends = new std::string[_capacity];
+    std::string *newFriends = new std::string[_capacity];
     for (size_t i = 0; i < _size; ++i) {
       newFriends[i] = _friends[i];
     }
@@ -34,32 +27,73 @@ User::add_friend(const std::string& name)
 /**
  * Returns the name of this User.
  */
-std::string
-User::get_name() const
-{
-  return _name;
-}
+std::string User::get_name() const { return _name; }
 
 /**
  * Returns the number of friends this User has.
  */
-size_t
-User::size() const
-{
-  return _size;
-}
+size_t User::size() const { return _size; }
 
 /**
  * Sets the friend at the given index to the given name.
  * @param index The index of the friend to set.
  * @param name The name to set the friend to.
  */
-void User::set_friend(size_t index, const std::string& name)
-{
+void User::set_friend(size_t index, const std::string &name) {
   _friends[index] = name;
 }
 
-/** 
+/**
  * STUDENT TODO:
- * The definitions for your custom operators and special member functions will go here!
+ * The definitions for your custom operators and special member functions will
+ * go here!
  */
+
+User::User(const User &user) {
+  _name = user._name;
+  _capacity = user._capacity;
+  _size = user._size;
+  if (_capacity > 0) {
+    _friends = new std::string[_capacity];
+    for (size_t i = 0; i < _size; i++) {
+      _friends[i] = user._friends[i];
+    }
+  } else {
+    _friends = nullptr;
+  }
+}
+
+User &User::operator=(const User &user) {
+  if (this != &user) {
+    User temp(user);
+    temp.swap(*this);
+  }
+  return *this;
+}
+
+User::~User() { delete[] _friends; }
+
+std::ostream &operator<<(std::ostream &os, const User &user) {
+  os << "User(name=" << user._name << ", friends=[";
+  if (user._size > 0) {
+    os << user._friends[0];
+  }
+  for (size_t i = 1; i < user._size; i++) {
+    os << ", " << user._friends[i];
+  }
+  os << "])";
+  return os;
+}
+
+User &User::operator+=(User &user) {
+  if (this == &user) {
+    return *this;
+  }
+
+  this->add_friend(user._name);
+  user.add_friend(this->_name);
+
+  return *this;
+}
+
+bool User::operator<(const User &rhs) const { return _name < rhs._name; }
--- assignment5/user.h
+++ assignment5/user.h
@@ -6,23 +6,40 @@
 #include <iostream>
 #include <string>
 
-class User
-{
+class User {
+  friend std::ostream &operator<<(std::ostream &os, const User &user);
+
 public:
-  User(const std::string& name);
-  void add_friend(const std::string& name);
+  User(const std::string &name);
+  void add_friend(const std::string &name);
   std::string get_name() const;
   size_t size() const;
-  void set_friend(size_t index, const std::string& name);
+  void set_friend(size_t index, const std::string &name);
 
-  /** 
+  /**
    * STUDENT TODO:
    * Your custom operators and special member functions will go here!
    */
+  User(const User &user);
+  User &operator=(const User &user);
+  ~User();
+  void swap(User &user) noexcept {
+    using std::swap;
+    swap(_name, user._name);
+    swap(_friends, user._friends);
+    swap(_size, user._size);
+    swap(_capacity, user._capacity);
+  }
+
+  User(User &&user) = delete;
+  User &operator=(User &&user) = delete;
+
+  User &operator+=(User &user);
+  bool operator<(const User &rhs) const;
 
 private:
   std::string _name;
-  std::string* _friends;
+  std::string *_friends;
   size_t _size;
   size_t _capacity;
 };
